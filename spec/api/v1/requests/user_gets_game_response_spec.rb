d1a6236014677b624990d06deb906dd3d231cde3
Mesky <dmeskis@gmail.com>
2018-10-30 11:47:17 -0600

    Fixed a test

--- spec/api/v1/requests/user_gets_game_response_spec.rb
+++ spec/api/v1/requests/user_gets_game_response_spec.rb
@@ -5,7 +5,7 @@ describe 'user hits an api endpoint for game' do
     @josh = User.create(id: 1, name: "Josh")
     @sal = User.create(id: 2, name: "Sal")
 
-    @game = Game.create(player_1: @josh, player_2: @sal)
+    @game = Game.create(id: 1, player_1: @josh, player_2: @sal)
 
     @josh.plays.create(game: @game, word: "sal", score: 3)
     @josh.plays.create(game: @game, word: "zoo", score: 12)
@@ -13,9 +13,9 @@ describe 'user hits an api endpoint for game' do
     @sal.plays.create(game: @game, word: "no", score: 2)
   end
   it 'returns a JSON response with data for that game' do
-
+    binding.pry
     json_response = File.open("./fixtures/game.json")
-    stub_const(:get, "https://localhost:3000/api/v1/games/1").
+    stub_request(:get, "https://localhost:3000/api/v1/games/1").
     to_return(status: 200, body: json_response)
 
     get '/api/v1/games/1'
