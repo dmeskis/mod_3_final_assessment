52f8aed54877e70ef12fb040470522b0f420fdf6
Mesky <dmeskis@gmail.com>
2018-10-30 10:06:02 -0600

    Retrieved sentence examples for canada and britain

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,27 @@ class WelcomeController < ApplicationController
   end
 
   def show
-    @word_facade = WordFacade.new(params[:word])
+    @word = params[:word]
+    conn = Faraday.new(:url => "https://od-api.oxforddictionaries.com/api/v1/") do |faraday|
+      faraday.headers["Content-Type"] = "application/json;charset=utf-8"
+      faraday.headers["app_id"] = "ff6a0588"
+      faraday.headers["app_key"] = "4abdab620cd9faa0c2b457208b9fd2cf"
+      faraday.adapter Faraday.default_adapter
+    end
+
+    # payload = {
+    #   "word": params[:word]
+    # }
+
+    response = conn.get("entries/en/#{@word}/sentences")
+    data = JSON.parse(response.body)
+    canadian = data["results"][0]["lexicalEntries"][0]["sentences"][1]
+    british = data["results"][0]["lexicalEntries"][0]["sentences"][4]
+    canadian_sentences = canadian["text"]
+    british_sentences = british["text"]
+
+
+    binding.pry
+    @word_facade = WordFacade.new(@word)
   end
 end
--- spec/features/guest_user_can_see_sentence_examples_spec.rb
+++ spec/features/guest_user_can_see_sentence_examples_spec.rb
@@ -9,6 +9,8 @@ describe 'guest user can see sentence examples' do
       click_on "Submit"
 
       expect(page).to have_content("Examples for using 'mindfulness'")
+      expect(page).to have_content("Meditation is one way to express mindfulness in a dedicated, concentrated manner.")
+      expect(page).to have_content("At such times, mindfulness of the practice of patience and the application of certain techniques will help us to continue generating this attitude.")
     end
   end
 end
