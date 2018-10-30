23db95bd7d77872411af7aecd1bfebd2acef57e8
Mesky <dmeskis@gmail.com>
2018-10-30 10:17:14 -0600

    User story 1 passes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,18 +4,7 @@ class WelcomeController < ApplicationController
 
   def show
     @word = params[:word]
-
-    service = OxfordService.new(word_params)
-
-    data = service.sentence_data
-    canadian = data[0]["lexicalEntries"][0]["sentences"][1]
-    british = data[0]["lexicalEntries"][0]["sentences"][4]
-    canadian_sentences = canadian["text"]
-    british_sentences = british["text"]
-
-
-    binding.pry
-    @word_facade = WordFacade.new(@word)
+    @word_facade = WordFacade.new(word_params)
   end
 
   private
